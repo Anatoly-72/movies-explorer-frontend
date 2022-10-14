7549a418d12f3912bdd96418cfe4f5ea9cdbbbe2
Anatoly Popov <ana23123@yandex.ru>
2022-10-14 21:19:54 +0500

    feat: добавить index.css

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+// import Logo from '../Logo/logo';
+// import Navigation from '../Navigation/Navigation';
+
+const Header = () => {
+   return (
+      <header className="header">
+         test
+      </header>
+   )
+}
+
+export default Header;
