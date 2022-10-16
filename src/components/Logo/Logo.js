d376dec8978307cdd076f65f0256b0cdd0428962
Anatoly Popov <ana23123@yandex.ru>
2022-10-16 22:20:32 +0500

    fix: изменить logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg'
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.svg";
 
 const Logo = () => {
-   return (
-      <Link to='/'>
-         <img className='logo' src={logo} alt='Логотип' />
-      </Link>
-   )
-
-}
+  return (
+    <Link to="/">
+      <img className="logo" src={logo} alt="Логотип" />
+    </Link>
+  );
+};
 
 export default Logo;
