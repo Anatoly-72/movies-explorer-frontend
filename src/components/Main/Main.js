f4b24d01c56587b2a92e953d0d1979de34a25793
Anatoly Popov <ana23123@yandex.ru>
2022-10-22 16:38:28 +0500

    feat: добавить AboutMe

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
-// import AboutMe from "./AboutMe/AboutMe";
+import AboutMe from "../AboutMe/AboutMe";
 // import Portfolio from "./Portfolio/Portfolio";
 
 function Main() {
@@ -10,8 +10,8 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
-      {/* <AboutMe />
-      <Portfolio /> */}
+      <AboutMe />
+      {/* <Portfolio /> */}
     </>
   );
 }
